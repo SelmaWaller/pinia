623f9e5f5442521605c94524ed40f51372a66de9
Selma <selma.waller@glasspaper.no>
2024-04-09 14:36:43 +0200

    css/accessing store state

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    // ...
-    '@pinia/nuxt',
-  ]
-})
+  modules: [// ...
+  "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+});
